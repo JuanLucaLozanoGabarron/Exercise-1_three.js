a8ddf88c2d6d0bb0039546084ab447a7ada9eba8
JuanLucaLozanoGabarron <juan-luca.lozano.gabarron@student.ehb.be>
2023-10-09 10:53:49 +0200

    Create vite-react app

---
+++ 3D Library/src/components/Box.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Box() {
+  return (
+    <div className='box'>
+        <div className="3D-image">
+
+        </div>
+        <div className="title">
+            <p>Juan</p>
+        </div>
+    </div>
+  )
+}
---
+++ 3D Library/src/components/Object.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Object() {
+  return (
+    <div>3Dimage</div>
+  )
+}
