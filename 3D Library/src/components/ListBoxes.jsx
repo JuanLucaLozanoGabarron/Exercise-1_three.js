08ffaa4a992a0ff7700fb7c9ec9eae569a363f46
JuanLucaLozanoGabarron <juan-luca.lozano.gabarron@student.ehb.be>
2023-10-10 14:38:05 +0200

    Added css to the components

--- 3D Library/src/components/ListBoxes.jsx
+++ 3D Library/src/components/ListBoxes.jsx
@@ -3,14 +3,19 @@ import Box from "./Box";
 import "./style/ListBoxes.css";
 import Car from "./../image/car.png";
 import Phone from "./../image/phone.png";
+import HotDog from "./../image/hotdog.png";
+import Sneaker from "./../image/sneaker.png";
 
 import { Link } from "react-router-dom";
 
 export default function ListBoxes() {
   return (
     <div className="list">
-      <Link to="/dog">
-        <Box image={Car} name="Golden Retriever" />
+      <Link to="/sneaker">
+        <Box image={Sneaker} name="Nike Dunk low" />
+      </Link>
+      <Link to="/sneaker">
+        <Box image={Sneaker} name="Nike Dunk low" />
       </Link>
       <Link to="/car">
         <Box image={Car} name="Car" />
@@ -18,7 +23,9 @@ export default function ListBoxes() {
       <Link to="/phone">
         <Box image={Phone} name="Vintage Phone" />
       </Link>
-      <Box image={Car} name="Human Skull" />
+      <Link to="/hotdog">
+        <Box image={HotDog} name="American Hot Dog" />
+      </Link>
     </div>
   );
 }
