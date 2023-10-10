a84ecd859f8acbf57a921926da6672c51baa35bd
JuanLucaLozanoGabarron <juan-luca.lozano.gabarron@student.ehb.be>
2023-10-10 12:37:45 +0200

    Create hotdog component and add 3D object

---
+++ 3D Library/src/routes/Hotdog.jsx
@@ -0,0 +1,33 @@
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import HotdogObject from "../../public/HotdogObject";
+import { Link } from "react-router-dom";
+import "./style/routes.css";
+
+export default function Car() {
+  return (
+    <div>
+      <Link to="/">
+        <div className="backButton">
+          <p>Go back</p>
+        </div>
+      </Link>
+
+      <Canvas
+        camera={{ position: [0, 0, 40], fov: 100 }}
+        style={{
+          height: "100vh",
+          background: "gray",
+        }}
+      >
+        <Suspense fallback={null}>
+          <ambientLight intensity={3} />
+          <directionalLight intensity={5} />
+          <HotdogObject />
+        </Suspense>
+        <OrbitControls />
+      </Canvas>
+    </div>
+  );
+}
