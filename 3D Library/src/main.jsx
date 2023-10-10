27224518bc483c03d06fa222dbba14f50d7d6c36
JuanLucaLozanoGabarron <juan-luca.lozano.gabarron@student.ehb.be>
2023-10-10 11:41:42 +0200

    Create routes using react router dom

--- 3D Library/src/main.jsx
+++ 3D Library/src/main.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Home from "./routes/Home";
+import Car from "./routes/Car";
+import Phone from "./routes/Phone";
+import Dog from "./routes/Dog";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/car",
+    element: <Car />,
+  },
+  {
+    path: "/phone",
+    element: <Phone />,
+  },
+  {
+    path: "/dog",
+    element: <Dog />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
