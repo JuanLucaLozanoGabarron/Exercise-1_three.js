1c4105989119d793780b1cb956ea8f71d98f130d
JuanLucaLozanoGabarron <juan-luca.lozano.gabarron@student.ehb.be>
2023-10-16 14:30:07 +0200

    Add animation using framer-motion

--- 3D Library/src/components/ListBoxes.jsx
+++ 3D Library/src/components/ListBoxes.jsx
@@ -7,25 +7,74 @@ import HotDog from "./../image/hotdog.png";
 import Sneaker from "./../image/sneaker.png";
 import TShirt from "./../image/tshirt2.png";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 export default function ListBoxes() {
+  const container = {
+    hidden: { opacity: 1, scale: 0 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        delayChildren: 0.3,
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
+  const item = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+    },
+  };
   return (
-    <div className="list">
-      <Link to="/sneaker">
-        <Box image={Sneaker} name="Nike Dunk low" />
-      </Link>
-      <Link to="/tshirt">
-        <Box image={TShirt} name="T-Shirt" />
-      </Link>
-      <Link to="/car">
-        <Box image={Car} name="Car" />
-      </Link>
-      <Link to="/phone">
-        <Box image={Phone} name="Vintage Phone" />
-      </Link>
-      <Link to="/hotdog">
-        <Box image={HotDog} name="American Hot Dog" />
-      </Link>
-    </div>
+    <motion.ul
+      className="container"
+      variants={container}
+      initial="hidden"
+      animate="visible"
+    >
+      {[0].map((index) => (
+        <div className="list">
+          <motion.div key={index} className="item" variants={item}>
+            <Link to="/sneaker">
+              <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+                <Box image={Sneaker} name="Nike Dunk low" />
+              </motion.div>
+            </Link>
+          </motion.div>
+          <motion.div key={index} className="item" variants={item}>
+            <Link to="/tshirt">
+              <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+                <Box image={TShirt} name="T-Shirt" />
+              </motion.div>
+            </Link>
+          </motion.div>
+          <motion.div key={index} className="item" variants={item}>
+            <Link to="/car">
+              <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+                <Box image={Car} name="Car" />
+              </motion.div>
+            </Link>
+          </motion.div>
+          <motion.div key={index} className="item" variants={item}>
+            <Link to="/phone">
+              <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+                <Box image={Phone} name="Vintage Phone" />
+              </motion.div>
+            </Link>
+          </motion.div>
+          <motion.div key={index} className="item" variants={item}>
+            <Link to="/hotdog">
+              <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+                <Box image={HotDog} name="American Hot Dog" />
+              </motion.div>
+            </Link>
+          </motion.div>
+        </div>
+      ))}
+    </motion.ul>
   );
 }
--- 3D Library/src/routes/Home.jsx
+++ 3D Library/src/routes/Home.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import ListBoxes from "../components/ListBoxes";
 import Header from "../components/Header";
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
     <div>
-      <Header />
+      <motion.div
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        transition={{
+          type: "spring",
+          stiffness: 140,
+          damping: 20,
+        }}
+      >
+        <Header />
+      </motion.div>
+
       <ListBoxes />
     </div>
   );
